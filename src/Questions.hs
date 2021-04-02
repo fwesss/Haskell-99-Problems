18e46821ee40c81e91d973fc2a34797e1272a1ba
Westley Feller <feller.westley@gmail.com>
2021-04-01 22:17:21 -0700

    Problems 20 - 28 completed

--- src/Questions.hs
+++ src/Questions.hs
@@ -1,6 +1,10 @@
+module Questions where
+
+import Data.List
+import System.Random
+
 -- Problem 1
 -- Find the last element of a list.
-import Data.List
 
 myLast :: [a] -> a
 myLast [x] = x
@@ -124,3 +128,67 @@ myRotate xs n
 -- Remove the K'th element from a list.
 myRemoveAt :: Int -> [a] -> (a, [a])
 myRemoveAt n xs = (myElementAt xs n, take (n - 1) xs ++ drop n xs)
+
+-- Problem 21
+-- Insert an element at a given position into a list.
+myInsertAt :: a -> [a] -> Int -> [a]
+myInsertAt y xs n = take (n - 1) xs ++ [y] ++ drop (n - 1) xs
+
+-- Problem 22
+-- Create a list containing all integers within a given range.
+myRange :: Int -> Int -> [Int]
+myRange n m = [n .. m]
+
+-- Problem 23
+-- Extract a given number of randomly selected elements from a list.
+myRandomSelect :: [a] -> Int -> IO [a]
+myRandomSelect xs n = do
+  gen <- getStdGen
+  return $ take n [xs !! x | x <- randomRs (0, length xs - 1) gen]
+
+-- Problem 24
+-- Lotto: Draw N different random numbers from the set 1..M.
+myDiffSelect :: (Random a, Num a) => Int -> a -> IO [a]
+myDiffSelect n m = do
+  gen <- getStdGen
+  return $ take n [x | x <- randomRs (0, m) gen]
+
+myRandomPermutation :: [a] -> IO [a]
+myRandomPermutation xs = do
+  myElementAt (permutations xs) . fst . random <$> getStdGen
+
+-- Problem 26
+-- (**) Generate the combinations of K distinct objects chosen from the N elements of a list
+-- In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3) = 220
+-- possibilities (C(N,K) denotes the well-known binomial coefficients). For pure mathematicians, this result may be
+-- great. But we want to really generate all the possibilities in a list.
+myCombinations :: Int -> [a] -> [[a]]
+myCombinations n = filter ((n ==) . length) . subsequences
+
+-- Problem 27
+-- Group the elements of a set into disjoint subsets.
+-- a) In how many ways can a group of 9 people work in 3 disjoint
+-- subgroups of 2, 3 and 4 persons? Write a function that generates all the possibilities and returns them in a list.
+-- b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a
+-- list of groups.
+myGroup :: (Foldable t, Eq a) => t Int -> [a] -> [[[a]]]
+myGroup ns xss = concatMap (\x -> myCombinations x (group xss)) ns
+
+-- Problem 28
+-- Sorting a list of lists according to length of sublists
+-- a) We suppose that a list contains elements that are lists themselves. The objective is to sort the elements of this
+-- list according to their length. E.g. short lists first, longer lists later, or vice versa.
+-- b) Again, we suppose that a list contains elements that are lists themselves. But this time the objective is to sort
+-- the elements of this list according to their length frequency; i.e., in the default, where sorting is done
+-- ascendingly, lists with rare lengths are placed first, others with a more frequent length come later.
+myLSort :: [[a]] -> [[a]]
+myLSort = sortOn length
+
+myCount :: Eq a => a -> [a] -> Int
+myCount x = length . filter (== x)
+
+myLFSort :: (Ord (t a), Foldable t) => [t a] -> [t a]
+myLFSort xss = [y | (_, y) <- sort withCount]
+  where
+    withLength = fmap (\x -> (length x, x)) xss
+    withCount = fmap (\(x, y) -> (myCount x [x | (x, _) <- withLength], y)) withLength
